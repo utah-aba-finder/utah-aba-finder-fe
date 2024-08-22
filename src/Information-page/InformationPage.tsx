9f3aca81e6c8fdf9c28f1f3f336fbbd9d95dd308
Seong Kang <seong.h.kang2@gmail.com>
2024-08-22 16:08:18 -0500

    iteration0/basic-structure-completed+Assets-uploaded

---
+++ src/Information-page/InformationPage.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+type Props = {};
+type State = {};
+
+class InformationPage extends Component<Props, State> {
+    state = {};
+
+    render() {
+        return (
+            <div>Information Page</div>
+        );
+    }
+}
+
+export default InformationPage;
---
+++ src/Login-page/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class LoginPage extends Component<Props, State> {
+    state = {}
+
+    render() {
+        return (
+            <div>LoginPage</div>
+        )
+    }
+}
