c85fdf9d5c16955665bf3f6ac6471ced00a9bd19
sanghoro <seong.h.kang2@gmail.com>
2024-08-22 17:27:11 -0500

    adding header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class Header extends Component<Props, State> {
+    state = {}
+
+    render() {
+        return (
+            <div>Header</div>
+        )
+    }
+}
