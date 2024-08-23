85b22aaf2e059e208f86f03c6b1e9f3ede19bb4a
sanghoro <seong.h.kang2@gmail.com>
2024-08-23 17:01:52 -0500

    iteration1/feat/homepage+added-begin-section+mobile-view

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import utah from '../Assets/utah.jpg'
+import utah from '../Assets/Utah-Background.jpg'
 import "./Homepage.css"
 
 type Props = {}
@@ -13,19 +13,21 @@ class Homepage extends Component<Props, State> {
         return (
             <div className='homepage-container'>
                 <div className='discover-section'>
-
                     <img src={utah} alt="background-image" className='discover-section-backgroundImage' />
-
-                    <h1 className='discover-section-title'>Discover the Ideal ABA Therapy Provider <br /> for Your Child with Autism in Utah</h1>
-
-                    <div className='discover-section-content'>
-                        <p className='discover-section-description'>Are you feeling overwhelmed in your search for the right care? We’re here to guide you every step of the way. Start your journey with confidence today.</p>
-                        <button className='discover-section-button'>Start Your Journey</button>
+                    <div className='discover-section-container'>
+                        <h1 className='discover-section-title'>
+                            Discover the Ideal ABA Therapy Provider <br /> for Your Child with Autism in Utah
+                        </h1>
+                        <div className='discover-section-content'>
+                            <p className='discover-section-description'>
+                                Are you feeling overwhelmed in your search for the right care? We’re here to guide you every step of the way. <br />Start your journey with confidence today.
+                            </p>
+                            <button className='discover-section-button'>Start Your Journey</button>
+                        </div>
                     </div>
-
                 </div>
                 <div className='county-section'>
-                    <h2 className='county-section-title'>We Cover These Counties</h2>
+                    <h2 className='county-section-title'>Counties Covered</h2>
                     <div className='county-section-list-wrapper'>
                         <div className='county-section-list'>
                             <p>Salt Lake <br /> County</p>
@@ -62,7 +64,15 @@ class Homepage extends Component<Props, State> {
                         </div>
                     </div>
                 </div>
-                <div className='begin-section'></div>
+                <div className='begin-section'>
+                    <h2 className='begin-section-title'>Begin Your Journey with Essential Information</h2>
+                    <p className='begin-section-description'>Not sure about your child’s condition or haven’t received a diagnosis yet? We’re here to assist you. Start by taking a simple test to see if your child meets the criteria.</p>
+                    <div className='begin-section-button-container'>
+                        <button className='begin-section-button1'>VIEW PROVIDERS</button>
+                        <button className='begin-section-button2'>HELP WITH DIAGNOSIS</button>
+                    </div>
+
+                </div>
                 <div className='icons-section'></div>
                 <div className='spanish-section'></div>
             </div>
