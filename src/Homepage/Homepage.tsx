0b7dea75c78241776ffd2b148cfd7d37c941bafc
sanghoro <seong.h.kang2@gmail.com>
2024-08-22 20:27:03 -0500

    finished discover-section

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import utah from '../Assets/utah.jpg'
+import "./Homepage.css"
 
 type Props = {}
 
@@ -11,9 +13,24 @@ class Homepage extends Component<Props, State> {
 
     render() {
         return (
-            <div>
+            <div className='homepage-container'>
                 <Header />
-                <h1>Homepage</h1>
+                <div className='discover-section'>
+
+                    <img src={utah} alt="background-image" className='discover-section-backgroundImage' />
+
+                    <h1 className='discover-section-title'>Discover the Ideal ABA Therapy Provider <br /> for Your Child with Autism in Utah</h1>
+
+                    <div className='discover-section-content'>
+                        <p className='discover-section-description'>Are you feeling overwhelmed in your search for the right care? We’re here to guide you every step of the way. Start your journey with confidence today.</p>
+                        <button className='discover-section-button'>Start Your Journey</button>
+                    </div>
+
+                </div>
+                <div className='county-section'></div>
+                <div className='begin-section'></div>
+                <div className='icons-section'></div>
+                <div className='spanish-section'></div>
                 <Footer />
             </div>
         )
