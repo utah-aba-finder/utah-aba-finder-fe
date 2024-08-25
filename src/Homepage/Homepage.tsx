243b3c268eaa8762f165a47d3f85e36529a4ec35
Kevin Nelson <km.nelson@outlook.com>
2024-08-24 20:23:00 -0400

    refactors

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -23,7 +23,7 @@ const Homepage: React.FC = () => {
                         <p className='discover-section-description'>
                             Are you feeling overwhelmed in your search for the right care? We’re here to guide you every step of the way. <br />Start your journey with confidence today.
                         </p>
-                        <button 
+                        <button
                             className='discover-section-button'
                             onClick={() => navigate('/providers')}
                         >
@@ -51,6 +51,38 @@ const Homepage: React.FC = () => {
                         <p>Uintah <br /> County</p>
                         <p>Sanpete <br /> County</p>
                         <p>Wayne <br /> County</p>
+
+                        <p>Salt Lake <br /> County</p>
+                        <p>Utah <br /> County</p>
+                        <p>Davis <br /> County</p>
+                        <p>Weber <br /> County</p>
+                        <p>Iron <br /> County</p>
+                        <p>Cache <br /> County</p>
+                        <p>Box Elder <br /> County</p>
+                        <p>Washington <br /> County</p>
+                        <p>Morgan <br /> County</p>
+                        <p>Summit <br /> County</p>
+                        <p>Tooele <br /> County</p>
+                        <p>Duchesne <br /> County</p>
+                        <p>Uintah <br /> County</p>
+                        <p>Sanpete <br /> County</p>
+                        <p>Wayne <br /> County</p>
+
+                        <p>Salt Lake <br /> County</p>
+                        <p>Utah <br /> County</p>
+                        <p>Davis <br /> County</p>
+                        <p>Weber <br /> County</p>
+                        <p>Iron <br /> County</p>
+                        <p>Cache <br /> County</p>
+                        <p>Box Elder <br /> County</p>
+                        <p>Washington <br /> County</p>
+                        <p>Morgan <br /> County</p>
+                        <p>Summit <br /> County</p>
+                        <p>Tooele <br /> County</p>
+                        <p>Duchesne <br /> County</p>
+                        <p>Uintah <br /> County</p>
+                        <p>Sanpete <br /> County</p>
+                        <p>Wayne <br /> County</p>
                     </div>
                 </div>
             </div>
@@ -58,7 +90,7 @@ const Homepage: React.FC = () => {
                 <h2 className='begin-section-title'>Begin Your Journey with Essential Information</h2>
                 <p className='begin-section-description'>Not sure about your child’s condition or haven’t received a diagnosis yet? <br />We’re here for you. Start by taking a simple test to see if your child meets the criteria.</p>
                 <div className='begin-section-button-container'>
-                    <button 
+                    <button
                         className='begin-section-button1'
                         onClick={() => navigate('/providers')}
                     >
