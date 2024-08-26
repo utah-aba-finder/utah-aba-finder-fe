25f63fa538f5cf61a21c2b5e2a5c6fe632363107
jordan williamson <williamsonjordan05@gmail.com>
2024-08-26 14:26:18 -0600

    updated sign up form

--- src/Provider-login/LoginPage.tsx
+++ src/Provider-login/LoginPage.tsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './LoginPage.css'
 import notes from '../Assets/behaviorPlan.jpg'
-
+import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react';
 
 export const LoginPage: React.FC = () => {
+        const [showPassword, setShorPassword] = useState(false);  
 
+        const handleShowPassword = () => {
+            setShorPassword(!showPassword);
+        }
 
         return (
             <div className='loginWrapper'>
-                <div className='loginImageContainer'>
-                    <img src={notes} alt='notepad' className='loginImage' />
-                    <h1 className='loginImageText'>Provider Login</h1>
-                </div>
                 <div className='loginContainer'>
+                    <img src={notes} alt='notepad' className='loginImage' />
+                    <h1 className='loginImageText'>Provider Sign Up</h1>
                     <form className='loginForm'>
-                        <label htmlFor='email'>Email:</label>
-                        <input type='email' id='email' name='email' required />
-                        <label htmlFor='password'>Password:</label>
-                        <input type='password' id='password' name='password' required />
-                        <button type='submit' className='loginButton'>Login</button>
+                        <div className='input'>
+                            <label htmlFor='username'>Provider Name (business):</label>
+                            <User className='userIcon'/>
+                            <input type='text' id='username' name='username' required />
+                        </div>
+                        <div className='input'> 
+                            <label htmlFor='email'>Email:</label>
+                            <Mail className='mailIcon'/>
+                            <input type='email' id='email' name='email' required />
+                        </div>
+                        <div className='input'>
+                            <label htmlFor='password'>Password:</label>
+                            <Lock className='lockIcon'/>
+                            <input type={showPassword ? 'text' : 'password'} id='password' name='password' required />
+                            <button className='eyeButton'type='button' onClick={handleShowPassword}>
+                                {showPassword ? <EyeOff className='eye'/> : <Eye className='eye'/>}
+                            </button>
+                        </div>
+                        <div className="forgot-password">Forgot Password <span>Click Here!</span></div>
+                        <div className="submit-container">
+                            <button type='submit' className='loginButton'>Sign Up</button>
+                            <button type='submit' className='loginButton'>Login</button>
+                        </div>
                     </form>
                 </div>
             </div>
--- src/Providers-page/SearchBar.tsx
+++ src/Providers-page/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; // Import CSS
+import 'react-toastify/dist/ReactToastify.css'; 
 import './SearchBar.css';
 
 interface SearchBarProps {
