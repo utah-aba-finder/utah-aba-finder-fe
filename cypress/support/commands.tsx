d6edf11ea7c94e08d6ae78f03789f19a81aef679
sanghoro <seong.h.kang2@gmail.com>
2024-09-19 18:55:32 -0500

    trying to have patch working

--- cypress/support/commands.tsx
+++ cypress/support/commands.tsx
@@ -1,27 +1,24 @@
 Cypress.Commands.add('setUpProvidersIntercepts', () => {
 <<<<<<< HEAD
 <<<<<<< HEAD
-    cy.intercept('GET', 'https://uta-aba-finder-be-97eec9f967d0.herokuapp.com/api/v1/providers', {
-      statusCode: 200,
-      fixture: 'providerspage.json',
-    }).as('fetchProviders');
-  });
-=======
-=======
->>>>>>> 6bc7399151c507ac204731cf8e82592cc1ea3022
-  // Intercept the API call for providers
   cy.intercept('GET', 'https://uta-aba-finder-be-97eec9f967d0.herokuapp.com/api/v1/providers', {
     statusCode: 200,
     fixture: 'providerspage.json',
   }).as('fetchProviders');
+});
+// Intercept the API call for providers
+cy.intercept('GET', 'https://uta-aba-finder-be-97eec9f967d0.herokuapp.com/api/v1/providers', {
+  statusCode: 200,
+  fixture: 'providerspage.json',
+}).as('fetchProviders');
 
-  cy.intercept('GET', '**/maps/embed/v1/place**', (req) => {
-    const address = req.query.q;
+cy.intercept('GET', '**/maps/embed/v1/place**', (req) => {
+  const address = req.query.q;
 
-    req.reply({
-      statusCode: 200,
-      headers: { 'content-type': 'text/html' },
-      body: `
+  req.reply({
+    statusCode: 200,
+    headers: { 'content-type': 'text/html' },
+    body: `
         <html>
           <body>
             <h1>Mock Google Map</h1>
@@ -29,10 +26,6 @@ Cypress.Commands.add('setUpProvidersIntercepts', () => {
           </body>
         </html>
       `,
-    });
-  }).as('googleMapsEmbed');
+  });
+}).as('googleMapsEmbed');
 });
-<<<<<<< HEAD
->>>>>>> 4fe5d0604359cd4a2d0fffb762ce84de991a4e8a
-=======
->>>>>>> 6bc7399151c507ac204731cf8e82592cc1ea3022
