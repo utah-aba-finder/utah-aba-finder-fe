f00846d8e522510da4cf7ab83d940b44ef54895d
jordan williamson <williamsonjordan05@gmail.com>
2024-10-07 14:21:28 -0600

    added message for contact us link on the provider edit page

--- src/Provider-edit/ProviderEdit.tsx
+++ src/Provider-edit/ProviderEdit.tsx
@@ -312,7 +312,7 @@ const ProviderEdit: React.FC<ProviderEditProps> = ({ loggedInProvider, clearProv
                 <h1>Welcome, {providerName}</h1>
                 <p>Last edited: {moment(loggedInProvider?.attributes.updated_last).utc().local().format('MM/DD/YYYY hh:mm:ss a')} {moment.tz.guess()}</p>
                 <button className='logoutButton' onClick={handleLogout}>Logout</button>
-                <p>For any questions to the admin, please use the contact page.</p>
+                <p>For any questions to the admin, please use the contact page. <strong>You will be logged out.</strong></p>
                 <Link to="/contact" className='contact-link' onClick={handleLogout}>Click here to go to the contact page</Link>
             </div>
 
