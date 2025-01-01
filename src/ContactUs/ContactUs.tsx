52ff39f3e2fd481c5228c44e8b6ab2e979130c75
jordan williamson <williamsonjordan05@gmail.com>
2024-12-31 17:30:22 -0700

    added interns to about us page and updated the contact us page with some more info

--- src/ContactUs/ContactUs.tsx
+++ src/ContactUs/ContactUs.tsx
@@ -39,7 +39,8 @@ const ContactUs: React.FC = () => {
                 <div className='contact-us-text'>
                     <p>If you have any questions or feedback, please feel free to contact us using the form below.</p>
                     <p>Or feel free to call us at <a href="tel:1(385)560-4481" className='contact-us-phone-number'>(385) 560-4481</a></p>
-                    <p>Our email is also: <a href="mailto:info@autismserviceslocator.com" className='contact-us-email'>info@autismserviceslocator.com</a></p>
+                    <p>Don't want to use the form? Email us at <a href="mailto:info@autismserviceslocator.com" className='contact-us-email'>info@autismserviceslocator.com</a></p>
+                    <p>Looking to be added to our database? Email us at <a href="mailto:register@autismserviceslocator.com" className='contact-us-email'>register@autismserviceslocator.com</a></p>
                 </div>
                 <form ref={form} className='contact-input-container' onSubmit={handleSubmit}>
                     <input name="user_name" placeholder='Your name' className='contact-input-name' required />
