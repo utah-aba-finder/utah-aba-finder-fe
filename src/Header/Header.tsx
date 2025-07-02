5c08ec576b52556da6a28d1f8350acbb834b1056
jordan williamson <williamsonjordan05@gmail.com>
2025-07-02 13:21:37 -0600

    logo updates

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ChevronDown } from "lucide-react";
-import Logo from "../Assets/NewHeaderLogo2.png";
+import Logo from "../Assets/FinalLogo.png";
 
 const navigationItems = [
   { name: "Home", href: "/" },
